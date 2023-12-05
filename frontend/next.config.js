45fc2cdcd18206d62ea8077da849dd0591197446
Festeiro <schenamarcelo@gmail.com>
2023-12-05 20:40:24 -0300

    changed to pnpm

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.dummyjson.com"]
-  }
-}
+    domains: ['i.dummyjson.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
